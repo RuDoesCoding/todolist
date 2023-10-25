1a91ba7a8a4f9a928fd2e305190b071e3f65e681
RuDoesCoding <148224331+RuDoesCoding@users.noreply.github.com>
2023-10-25 13:09:43 +0100

    Add files via upload
    51895616548

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<!-- src/App.vue -->
+<template>
+  <ion-app>
+    <ion-page>
+      <ion-datetime-button datetime="datetime"></ion-datetime-button>
+
+      <ion-modal :keep-contents-mounted="true">
+        <ion-datetime id="datetime"></ion-datetime>
+      </ion-modal>
+      <to-do-list :todos="todos" @remove-todo="removeTodo" />
+      <add-to-do @add-todo="addTodo" />
+    </ion-page>
+  </ion-app>
+</template>
+
+<script>
+import { IonApp, IonDatetime, IonDatetimeButton, IonModal } from '@ionic/vue';
+import ToDoList from './components/ToDoList.vue';
+import AddToDo from './components/AddToDo.vue';
+import { ref, defineComponent } from 'vue';
+export default {
+  components: {
+    AddToDo,
+    ToDoList,
+    IonApp,
+    IonDatetime,
+    IonDatetimeButton,
+    IonModal,
+  },
+  setup() {
+    const todos = ref([]);
+    const addTodo = (newTodo) => {
+      todos.value.push(newTodo);
+    };
+
+    const removeTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    return {
+      todos,
+      addTodo,
+      removeTodo,
+    };
+  },
+};
+</script>
